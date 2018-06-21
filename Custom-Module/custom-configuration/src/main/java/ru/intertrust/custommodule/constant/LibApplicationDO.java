74c558c06010c13bb3a36c48d98abb53a96ea705
Myskin Sergey <myskin.sergey@gmail.com>
2018-06-21 18:35:58 +0300

    Фикс формы заявок: фикс запроса; Добавление текущего юзера как автора заявки

---
+++ Custom-Module/custom-configuration/src/main/java/ru/intertrust/custommodule/constant/LibApplicationDO.java
@@ -0,0 +1,10 @@
+package ru.intertrust.custommodule.constant;
+
+/**
+ * Created by Myskin Sergey on 21.06.2018.
+ */
+public interface LibApplicationDO {
+
+    public static final String AUTHOR_FIELD = "author";
+
+}
--- Custom-Module/custom-configuration/src/main/java/ru/intertrust/custommodule/extensions/ApplicationReturnToExtensionPoint.java
+++ Custom-Module/custom-configuration/src/main/java/ru/intertrust/custommodule/extensions/ApplicationReturnToExtensionPoint.java
@@ -1,17 +1,32 @@
 package ru.intertrust.custommodule.extensions;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.intertrust.cm.core.business.api.dto.DomainObject;
 import ru.intertrust.cm.core.business.api.dto.FieldModification;
+import ru.intertrust.cm.core.business.api.dto.Id;
+import ru.intertrust.cm.core.dao.api.CurrentUserAccessor;
 import ru.intertrust.cm.core.dao.api.extension.BeforeSaveExtensionHandler;
 import ru.intertrust.cm.core.dao.api.extension.ExtensionPoint;
+import ru.intertrust.custommodule.constant.LibApplicationDO;
 
 import java.util.List;
 
 @ExtensionPoint(filter = "lib_application")
 public class ApplicationReturnToExtensionPoint implements BeforeSaveExtensionHandler {
+
+    @Autowired
+    private CurrentUserAccessor currentUserAccessor;
+
     @Override
     public void onBeforeSave(DomainObject domainObject, List<FieldModification> list) {
         final String typeName = domainObject.getTypeName();
         System.out.println("##### Before save" + typeName);
+
+        // Ссылка на системную сущность “Person”. Заполняется автоматически: Текущий пользователь. Менять нельзя.
+        final Id appAuthorId = domainObject.getReference(LibApplicationDO.AUTHOR_FIELD);
+        if (appAuthorId == null) {
+            Id currentUserId = currentUserAccessor.getCurrentUserId();
+            domainObject.setReference(LibApplicationDO.AUTHOR_FIELD, currentUserId);
+        }
     }
 }
